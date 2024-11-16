be3a0a99e1833b77341f8eff6e2342a640fe5723
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-11-16 21:16:56 +0000

    Add basic config validation
    By hand for now since the config is very simple. If it grows, perhaps a
    dedicated crate could be used for this.

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use std::fs;
+use std::{fs, process};
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -17,6 +17,22 @@ impl Config {
         // TODO: Proper error handling comes later
         let config_content = fs::read_to_string(path).unwrap();
         let config: Self = toml::from_str(&config_content).unwrap();
+
+        // Validation
+        let mut errors: Vec<&str> = vec![];
+        // More sophisticated URL validation could be done here, but this will do for now.
+        if !config.core.gitlab_url.starts_with("http://")
+            || !config.core.gitlab_url.starts_with("https://")
+        {
+            errors.push("Config 'core.gitlab_url' must be a valid URL");
+        }
+        if config.core.gitlab_projects.len() == 0 {
+            errors.push("Config 'core.gitlab_projects' needs to define at least one project");
+        }
+        if errors.len() > 0 {
+            eprintln!("Invalid config. Errors:\n{}", errors.join("\n"));
+            process::exit(1);
+        }
         config
     }
 }

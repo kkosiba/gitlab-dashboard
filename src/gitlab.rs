f8b3d1d3f7a68edb0292d29fc9bdb45d1fdae210
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-11-24 21:02:58 +0000

    Update structs used in gitlab module

--- src/gitlab.rs
+++ src/gitlab.rs
@@ -1,33 +1,20 @@
-use std::fmt;
+use std::{env, error::Error};
+
+use gitlab::{
+    api::{
+        self,
+        projects::pipelines::{PipelineSource, PipelineStatus, Pipelines},
+        Pagination, Query,
+    },
+    Gitlab,
+};
 
 #[derive(Debug)]
-pub enum PipelineStatus {
-    Created,
-    WaitingForResource,
-    Preparing,
-    Pending,
-    Running,
-    Success,
-    Failed,
-    Canceled,
-    Skipped,
-    Manual,
-    Scheduled,
-}
-
-impl fmt::Display for PipelineStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-pub struct Pipeline {
+pub struct GitlabPipeline {
     pub id: u32,
     pub status: PipelineStatus,
-    /*
-    We could define 'source' using enum, using data available here:
-    https://docs.gitlab.com/ee/ci/jobs/job_rules.html#ci_pipeline_source-predefined-variable
-    but at this point, let's keep it simple.
-    */
-    pub source: String,
+    pub source: PipelineSource,
+}
+
+
 }

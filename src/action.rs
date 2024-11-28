bf52db3f048528576adc4ca31203401906a8bd9b
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-11-28 20:25:32 +0000

    Add action module

---
+++ src/action.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+use strum::Display;
+
+#[derive(Debug, Clone, PartialEq, Eq, Display, Serialize, Deserialize)]
+pub enum Action {
+    Tick,
+    Render,
+    Quit,
+    Error(String),
+    Help,
+}

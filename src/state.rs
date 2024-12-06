5f5e6359561fb67f414888f18516e09462f958a6
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-12-06 21:49:37 +0000

    Add missing State field

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,6 @@
 #[derive(Default)]
 pub struct State {
+    pub active_gitlab_project: String,
     pub active_operation_index: usize,
     pub active_filter: String,
     pub input_mode: InputMode,

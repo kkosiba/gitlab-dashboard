30fa03d727b0fdce898a553a9242b84d487c7850
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-12-07 10:31:59 +0000

    Redesign project selector component
    - use global state
    - use prepare_layout to place it on the left column
    - various minor simplifications

--- src/components/project_selector_component.rs
+++ src/components/project_selector_component.rs
@@ -2,21 +2,17 @@ use color_eyre::Result;
 use ratatui::{prelude::*, widgets::*};
 use tokio::sync::mpsc::UnboundedSender;
 
-use super::{utils::centered_layout, Component};
-use crate::{action::Action, config::Config};
-
-#[derive(Default)]
-struct ProjectSelectorState {
-    active_operation_index: usize,
-    active_project: Option<String>,
-    should_render: bool,
-}
+use super::{
+    utils::{prepare_layout, Body, Element},
+    Component,
+};
+use crate::{action::Action, config::Config, state::State};
 
 #[derive(Default)]
 pub struct ProjectSelectorComponent {
     command_tx: Option<UnboundedSender<Action>>,
     config: Config,
-    state: ProjectSelectorState,
+    active_operation_index: usize,
 }
 
 impl ProjectSelectorComponent {
@@ -24,35 +20,30 @@ impl ProjectSelectorComponent {
         Self::default()
     }
 
-    fn select_project(&mut self) {
-        let projects = &self.config.core.gitlab_projects;
-        // At this point we're guaranteed to have at least one GitLab project in the config file.
-        if projects.len() > 1 {
-            self.state.should_render = true;
-        }
-        self.state.active_project = Some(projects[0].to_string());
-    }
-
     fn next(&mut self) {
         let projects = &self.config.core.gitlab_projects;
-        if self.state.active_operation_index < projects.len() - 1 {
-            self.state.active_operation_index += 1;
+        if self.active_operation_index < projects.len() - 1 {
+            self.active_operation_index += 1;
         }
     }
 
     fn previous(&mut self) {
-        if self.state.active_operation_index > 0 {
-            self.state.active_operation_index -= 1;
+        if self.active_operation_index > 0 {
+            self.active_operation_index -= 1;
         }
     }
 
-    fn confirm(&mut self) {
+    fn select_project(&mut self, state: &mut State) {
         let projects = &self.config.core.gitlab_projects;
-        self.state.active_project = Some(projects[self.state.active_operation_index].clone());
+        state.active_gitlab_project = Some(projects[self.active_operation_index].clone());
     }
 }
 
 impl Component for ProjectSelectorComponent {
+    fn height_constraint(&self) -> Constraint {
+        Constraint::Fill(3)
+    }
+
     fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<()> {
         self.command_tx = Some(tx);
         Ok(())
@@ -63,48 +54,39 @@ impl Component for ProjectSelectorComponent {
         Ok(())
     }
 
-    fn update(&mut self, action: Action) -> Result<Option<Action>> {
+    fn update(&mut self, action: Action, state: &mut State) -> Result<Option<Action>> {
         match action {
             Action::Next => self.next(),
             Action::Previous => self.previous(),
-            Action::Enter => self.confirm(),
-            Action::Tick => {
-                // add any logic here that should run on every tick
-            }
-            Action::Render => self.select_project(),
+            Action::Enter => self.select_project(state),
             _ => {}
         }
         Ok(None)
     }
 
-    fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
-        if !self.state.should_render {
-            Ok(())
-        } else {
-            let area = centered_layout(area);
-            let projects = &self.config.core.gitlab_projects;
-            let list_items: Vec<ListItem> =
-                projects.iter().map(|i| ListItem::new(i.clone())).collect();
+    fn draw(&mut self, frame: &mut Frame, area: Rect, _state: &State) -> Result<()> {
+        let area = prepare_layout(area, Element::Body(Body::LeftColumn));
+        let projects = &self.config.core.gitlab_projects;
+        let list_items: Vec<ListItem> = projects.iter().map(|i| ListItem::new(i.clone())).collect();
 
-            let mut list_state = ListState::default();
-            list_state.select(Some(self.state.active_operation_index));
+        let mut list_state = ListState::default();
+        list_state.select(Some(self.active_operation_index));
 
-            let list = List::new(list_items)
-                .block(
-                    Block::default()
-                        .borders(Borders::ALL)
-                        .title("GitLab Project Selector")
-                        .title_alignment(Alignment::Center),
-                )
-                .highlight_style(
-                    Style::default()
-                        .fg(Color::Black)
-                        .bg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                );
+        let list = List::new(list_items)
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title("GitLab Project Selector")
+                    .title_alignment(Alignment::Center),
+            )
+            .highlight_style(
+                Style::default()
+                    .fg(Color::Black)
+                    .bg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            );
 
-            frame.render_stateful_widget(list, area, &mut list_state);
-            Ok(())
-        }
+        frame.render_stateful_widget(list, area, &mut list_state);
+        Ok(())
     }
 }

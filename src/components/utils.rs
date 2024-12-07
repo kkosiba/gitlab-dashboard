ecc10ed6a0e2b3cb1c80860b2e4b21d8300243e9
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-12-07 10:27:40 +0000

    Redesign layout split through prapare_layout function

--- src/components/utils.rs
+++ src/components/utils.rs
@@ -1,28 +1,39 @@
 use ratatui::layout::{Constraint, Direction, Layout, Rect};
 
-pub fn centered_layout(area: Rect) -> Rect {
-    let vertical_chunks = Layout::default()
+pub enum Body {
+    LeftColumn,
+    RightColumn,
+}
+
+pub enum Element {
+    Header,
+    Body(Body),
+    Footer,
+}
+
+pub fn prepare_layout(area: Rect, position: Element) -> Rect {
+    // First, we do the vertical split into header, body and footer
+    let chunks = Layout::default()
         .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Percentage(40), // Space above the widget
-                Constraint::Percentage(20), // Space for the widget
-                Constraint::Percentage(40), // Space below the widget
-            ]
-            .as_ref(),
-        )
+        .constraints([Constraint::Max(1), Constraint::Fill(1), Constraint::Max(1)])
         .split(area);
 
-    let horizontal_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage(40), // Space to the left of the widget
-                Constraint::Percentage(20), // Space for the widget
-                Constraint::Percentage(40), // Space to the right of the widget
-            ]
-            .as_ref(),
-        )
-        .split(vertical_chunks[1]);
-    horizontal_chunks[1]
+    // Then, depending on the position, we return the chunk which should be used to render specific
+    // component
+    match position {
+        Element::Header => chunks[0],
+        Element::Footer => chunks[2],
+        Element::Body(body_position) => {
+            // Here we split the main body into two columns
+            let body_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(15), Constraint::Percentage(85)])
+                .split(chunks[1]);
+
+            match body_position {
+                Body::LeftColumn => body_chunks[0],
+                Body::RightColumn => body_chunks[1],
+            }
+        }
+    }
 }

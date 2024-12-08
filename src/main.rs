b66fe1eea0f67b40c7a00ee529c7f05556263855
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-12-08 18:13:48 +0000

    Add panic handler
    Otherwise it's very hard to debug this application when it panics with
    no details.

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,58 @@ use cli::Cli;
 use config::Config;
 
 use color_eyre::Result;
+use tracing::error;
+
+pub fn initialize_panic_handler() -> Result<()> {
+    let (_, eyre_hook) = color_eyre::config::HookBuilder::default()
+        .panic_section(format!(
+            "This is a bug. Consider reporting it at {}",
+            env!("CARGO_PKG_REPOSITORY")
+        ))
+        .capture_span_trace_by_default(false)
+        .display_location_section(false)
+        .display_env_section(false)
+        .into_hooks();
+    eyre_hook.install()?;
+    std::panic::set_hook(Box::new(move |panic_info| {
+        if let Ok(mut t) = crate::tui::Tui::new() {
+            if let Err(r) = t.exit() {
+                error!("Unable to exit Terminal: {:?}", r);
+            }
+        }
+
+        #[cfg(not(debug_assertions))]
+        {
+            use human_panic::{handle_dump, print_msg, Metadata};
+            let meta = Metadata::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+                .authors(env!("CARGO_PKG_AUTHORS").replace(':', ", "))
+                .homepage(env!("CARGO_PKG_HOMEPAGE"));
+
+            let file_path = handle_dump(&meta, panic_info);
+            // prints human-panic message
+            print_msg(file_path, &meta)
+                .expect("human-panic: printing error message to console failed");
+            eprintln!("{}", panic_hook.panic_report(panic_info)); // prints color-eyre stack trace to stderr
+        }
+        #[cfg(debug_assertions)]
+        {
+            // Better Panic stacktrace that is only enabled when debugging.
+            better_panic::Settings::auto()
+                .most_recent_first(false)
+                .lineno_suffix(true)
+                .verbosity(better_panic::Verbosity::Full)
+                .create_panic_handler()(panic_info);
+        }
+
+        std::process::exit(1);
+    }));
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    initialize_panic_handler()?;
+
     let args = Cli::parse();
     let config = Config::new(args.config_file)?;
     let mut app = App::new(config)?;
